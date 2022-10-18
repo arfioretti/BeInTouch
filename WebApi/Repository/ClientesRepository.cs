49ed17188c81fd6f6cb54976ada17bff534974de
aristides fioretti <arfioretti@gmail.com>
2022-10-18 20:22:40 -0300

    crud

---
+++ WebApi/Repository/ClientesRepository.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using WebApi.Context;
+using WebApi.Models.Entity;
+
+namespace WebApi.Repository
+{
+    public class ClientesRepository : IClientesRepository
+    {
+        private BeInTouchContext db = new BeInTouchContext();
+
+
+        public bool ClienteExist(int id)
+        {
+            return db.Clientes.Count(e => e.Id == id) > 0;
+        }
+
+        public bool DeleteCliente(int id)
+        {
+            Cliente cliente = db.Clientes.Find(id);
+
+            if (cliente == null)
+            {
+                return false;
+            }
+
+            db.Clientes.Remove(cliente);
+            db.SaveChanges();
+
+            return true;
+        }
+
+        public Cliente GetCliente(int id)
+        {
+            Cliente cliente = db.Clientes.Find(id);
+
+            return cliente;
+        }
+
+        public IQueryable<Cliente> GetClientes()
+        {
+            return db.Clientes;
+        }
+
+        public bool PostCliente([FromBody] Cliente cliente)
+        {
+            db.Clientes.Add(cliente);
+            db.SaveChanges();
+
+            return true;
+        }
+
+        public bool PutCliente(int id, [FromBody] Cliente cliente)
+        {
+            db.Entry(cliente).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return false;
+            }
+
+            return true;
+        }
+        public void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+        }
+    }
+}
---
+++ WebApi/Repository/IClientesRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Web.Http;
+using WebApi.Models.Entity;
+
+namespace WebApi.Repository
+{
+    public interface IClientesRepository
+    {
+        bool ClienteExist(int id);
+        bool DeleteCliente(int id);
+        Cliente GetCliente(int id);
+        IQueryable<Cliente> GetClientes();
+        bool PostCliente([FromBody] Cliente cliente);
+        bool PutCliente(int id, [FromBody] Cliente cliente);
+
+        void Dispose(bool disposing);
+    }
+}
